78eac3cf60d4bdf439379fe7dc8dfb80e1c812ba
Alexander <xander1620@gmail.com>
2021-09-09 22:57:51 -0400

    detalle

--- Pejemplo01/main.cpp
+++ Pejemplo01/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main() {
 	cout << "Sis-457" << endl;
 	cout << "Nombre: Marlon Alexander Morodias Bohorquez" << endl;
-	cout << "Número: 61891740" << endl << "CI: 8534011" << endl << "CU: 35-4267";
+	cout << "Numero: 61891740" << endl << "CI: 8534011" << endl << "CU: 35-4267";
 	return 0;
 }
